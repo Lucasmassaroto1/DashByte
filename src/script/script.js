273ee95825855340d39f3dfeff648e888af19342
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-23 11:47:25 -0300

    adicionando card prefixo

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,6 @@
+/* 
+    Bloco que atualiza o status do bot no card
+*/
 const statusBot = "online";
 const statusSpan = document.querySelector(".card-body .status");
 
@@ -5,3 +8,47 @@ statusSpan.textContent = statusBot === "online" ? "Online" : "Offline";
 statusSpan.classList.remove("online", "offline");
 statusSpan.classList.add(statusBot);
 
+/* 
+    Bloco que atualiza o prefixo do bot no card
+    mantendo o prefixo original
+*/
+document.addEventListener("DOMContentLoaded", () =>{
+    const input = document.querySelector(".input-prefix");
+    const prefixOriginal = document.querySelector(".status-prefix.online");
+    const prefixCustomizado = document.querySelector(".status-prefix.offline");
+
+    input.addEventListener("keydown", (event) =>{
+        if (event.key === "Enter" && input.value.trim() !== ""){
+            const novoPrefixo = input.value.trim();
+            prefixCustomizado.textContent = novoPrefixo;
+            prefixOriginal.classList.remove("online");
+            prefixOriginal.classList.add("offline");
+            prefixCustomizado.classList.remove("offline");
+            prefixCustomizado.classList.add("online");
+            input.value = "";
+        }
+    });
+});
+/* 
+    CODIGO FUNCIONANDO
+const inputPrefix = document.getElementById('input-prefix');
+const originalPrefix = document.getElementById('original-prefix');
+const customPrefix = document.getElementById('custom-prefix');
+
+inputPrefix.addEventListener('input', () =>{
+    const newPrefix = inputPrefix.value.trim();
+
+    if (newPrefix !== ""){
+        customPrefix.textContent = newPrefix;
+        originalPrefix.classList.remove('online');
+        originalPrefix.classList.add('offline');
+        customPrefix.classList.remove('offline');
+        customPrefix.classList.add('online');
+    } else{
+        customPrefix.textContent = "-";
+        originalPrefix.classList.remove('offline');
+        originalPrefix.classList.add('online');
+        customPrefix.classList.remove('online');
+        customPrefix.classList.add('offline');
+    }
+}); */
